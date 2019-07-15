d722c23efde19431b869410356ec3c8581615c1a
MilanZlaja1986 <milan.zlatkovic@hotmail.rs>
2019-07-15 11:51:20 +0200

    EncryptorTest code added

--- test/exercises/PalindromeTest.java
+++ test/exercises/PalindromeTest.java
@@ -14,7 +14,26 @@ class PalindromeTest {
 	}
 	
 	// 2. Test that a word exists in the dictionary 
+	@Test
+	void testWordApears() throws Exception{
+		assertTrue(testPalindrome.loadWords().contains("abacist"));
+		assertTrue(testPalindrome.loadWords().contains("acetonaphthone"));		
+		assertTrue(testPalindrome.loadWords().contains("extracalicular"));
+		
+		assertFalse(testPalindrome.loadWords().contains("fervider"));
+	}
+	
+	
 	
 	// 3. Test that a word is a palindrome
+	@Test
+	void testIsWordPalindrome() throws Exception{
+		assertTrue(testPalindrome.isWordPalindrome("madam"));
+		assertTrue(testPalindrome.isWordPalindrome("aba"));		
+		assertTrue(testPalindrome.isWordPalindrome("civic"));
+		
+		assertFalse(testPalindrome.isWordPalindrome("civilly"));
+	}
+	
 
 }
